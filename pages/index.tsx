499d20460940389133d0fe90fff5c17cc702caec
Ahljenn <ahljennm2@gmail.com>
2022-08-07 13:20:35 -0700

    initial styling home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,22 @@
 import type { NextPage } from 'next';
+
 const Home: NextPage = (): JSX.Element => {
-  return <h1 className="text-3xl font-bold">Hello world!</h1>;
+  return (
+    <>
+      <section className="mt-5 text-center max-w-screen-2xl">
+        <h1 className="text-[3.5rem] font-bold">The Nutrition Guide for Your Best Self</h1>
+        <h1>
+          <b>Metabolite </b>present to you unbiased nutrition. We aim to provide well-researched
+          guides catered to anyone.
+        </h1>
+
+        <div className="mt-5 flex gap-5 justify-center">
+          <button className="px-8 py-1 bg-tertiary rounded-xl shadow-xl">Read the Research</button>
+          <button className="px-8 py-2 bg-secondary rounded-xl shadow-xl">Read the Recipes</button>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        primary: '#399E5A',
+        secondary: '#A6CFD5',
+        tertiary: '#F6E27F',
+        metablack: '#232528',
+        metawhite: '#EAF6FF',
+      },
+    },
   },
   plugins: [],
 };
