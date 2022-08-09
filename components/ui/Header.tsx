05d375e58a155c406d252132e73d9a9e04c5f208
Ahljenn <ahljennm2@gmail.com>
2022-08-08 22:42:13 -0700

    Styling for header

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -6,14 +6,14 @@ const Header: React.FC = (): JSX.Element => {
     <>
       <header className="sticky top-0 z-50 bg-metablack py-2">
         <div className="flex flex-col sm:flex-row justify-between mx-5 items-center">
-          <div className="flex items-center gap-5">
+          <div className="hidden sm:flex items-center sm:gap-2">
             <picture>
-              <img className="w-[2.5rem]" src="./logo.png" alt="logo" />
+              <img className="w-[2rem]" src="./logo.png" alt="logo" />
             </picture>
-            <h1 className="text-white text-xl">Metabolite</h1>
+            <h1 className="text-secondary text-xl sm:visible">Metabolite</h1>
           </div>
           <button
-            className="text-white bg-black py-2 px-5 rounded-xl hover:bg-white hover:text-black transition duration-300"
+            className="text-gray-400 px-2 rounded-xl hover:text-white transition duration-300"
             onClick={(): void => {
               Router.push('/');
             }}
