a5620db541c30e1ac1ac91f8a97c47a08fdb641d
Ahljenn <ahljennm2@gmail.com>
2022-08-14 21:45:49 -0700

    Added table for things to eat

--- components/static_data/food_items.ts
+++ components/static_data/food_items.ts
@@ -1,8 +1,45 @@
-export const good_foods: String[] = [
-  'Cheese',
-  'Butter',
-  'Eggs',
-  'Bacon',
-  'Breakfast Sausages',
-  'Bratwursts',
+export interface FoodItem {
+  name: string;
+  source?: string;
+  vit_min?: string;
+  info?: string;
+}
+
+export const good_foods: FoodItem[] = [
+  {
+    name: 'Cheese',
+    source: 'Protein',
+    vit_min: 'Calcium',
+    info: 'N/A',
+  },
+  {
+    name: 'Butter',
+    source: 'Protein',
+    vit_min: 'Vitamin D',
+    info: 'Use for cooking',
+  },
+  {
+    name: 'Eggs',
+    source: 'Protein',
+    vit_min: 'Calcium',
+    info: 'N/A',
+  },
+  {
+    name: 'Bacon',
+    source: 'Fat',
+    vit_min: 'Calcium',
+    info: 'N/A',
+  },
+  {
+    name: 'Breakfast Sausage',
+    source: 'Protein',
+    vit_min: 'Vitamin B-12, Iron',
+    info: 'N/A',
+  },
+  {
+    name: 'Bratwurst',
+    source: 'Protein',
+    vit_min: 'Thiamin',
+    info: 'N/A',
+  },
 ];
--- pages/what-can-i-eat/index.tsx
+++ pages/what-can-i-eat/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { good_foods } from '../../components/static_data/food_items';
+import { good_foods, FoodItem } from '../../components/static_data/food_items';
 
 const WhatToEat: React.FC = (): JSX.Element => {
   return (
     <div className="flex items-center flex-col ">
       <section className="text-center max-w-screen-2xl">
-        <h1 className="mx-5 text-[2rem] sm:text-[3.5rem] font-bold">What are things I can eat?</h1>
+        <h1 className="mx-5 text-[1.3rem] sm:text-[2rem] md:text-[3rem] font-bold">
+          What are things I can eat?
+        </h1>
         <div className="flex justify-center">
           <p className="w-1/2">
             After walking around the grocery store and looking at food labels, you may be very
@@ -14,9 +16,45 @@ const WhatToEat: React.FC = (): JSX.Element => {
           </p>
         </div>
 
-        {good_foods.map((food: String, index: number) => {
-          return <div key={index}>{food}</div>;
-        })}
+        <div className="flex justify-center mt-5">
+          <table className="table-auto">
+            <caption>Foods you can eat</caption>
+            <thead className="text-sm sm:text-md bg-metawhite">
+              <tr>
+                <th className="py-3 sm:px-6" scope="col">
+                  Food
+                </th>
+                <th className="py-3 px-6" scope="col">
+                  Source
+                </th>
+                <th className="py-3 sm:px-6" scope="col">
+                  Vitamins & Minerals
+                </th>
+                <th className="py-3 sm:px-6" scope="col">
+                  Info
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {good_foods.map((food: FoodItem, index: number) => {
+                return (
+                  <tr className="border-b" key={index}>
+                    <th className="py-4 sm:px-6 text-gray-600 font-medium text-md" scope="row">
+                      {food.name}
+                    </th>
+                    {food.source === 'Fat' ? (
+                      <td className="text-yellow-500 py-4 sm:px-6">{food.source}</td>
+                    ) : (
+                      <td className="text-gray-500 py-4 sm:px-6">{food.source}</td>
+                    )}
+                    <td className="text-gray-500 py-4 sm:px-6">{food.vit_min}</td>
+                    <td className="text-gray-500 py-4 sm:px-6">{food.info}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </section>
     </div>
   );
