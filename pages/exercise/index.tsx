992bf0371b09eb711e1fc4a122b25bdc9d3cee3f
Ahljenn <ahljennm2@gmail.com>
2022-09-14 14:20:29 -0700

    Added excercise section

--- pages/exercise/index.tsx
+++ pages/exercise/index.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import Head from 'next/head';
 
-const Exercise: React.FC = (): JSX.Element => {
+const Excercise: React.FC = (): JSX.Element => {
   return (
     <>
-      <h1 className="mx-5 text-[2rem] sm:text-[3.5rem] font-bold text-center">Come back later!</h1>
+      <Head>
+        <title>Excercise | Metabolite</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <section className="flex flex-col items-center mt-5 ">
+        <h2 className="font-bold text-md sm:text-2xl text-primary mt-5">Excercise</h2>
+        <p className="mt-5 w-[50rem]">
+          Do you have to exercise often for this diet? Absolutely not! You don’t have to go to the
+          gym everyday or run around the block a thousand times. However, if you really want to
+          increase the effectiveness of this diet, it is highly recommended that you at least be
+          lightly active. You can run maybe a few times a week and lift weights a few times a week.
+          Better yet, if you’re the athletic or fitness type, work out a lot! Again though, don’t
+          worry about exercising too much. Just try not to be a couch potato.
+        </p>
+        <h3 className="mt-5 font-bold text-lg">Ultimately, calories in vs. calories out</h3>
+        <p className="mt-5 w-[50rem]">
+          The goal is to burn the more calories than you intake. If you are unable to excercise for
+          any reason, simply in take less calories. Your body already burns calories by simply
+          sustaining itself. See the section of <b>basal metabolic rate</b> for more information on
+          this.
+        </p>
+      </section>
     </>
   );
 };
 
-export default Exercise;
+export default Excercise;
