75a01593f8d5f210b40bdadad3839cbdbae3b9e4
Ahljenn <ahljennm2@gmail.com>
2022-11-24 20:11:21 -0800

    keto flu page

---
+++ components/static_data/flu_symptoms.ts
@@ -0,0 +1,8 @@
+export const symptoms: string[] = [
+  'Lack of energy, lethargy, fatigue',
+  'Brain fog',
+  'Nausea',
+  'Stomach aches',
+  'Constipation',
+  'Weakness',
+];
--- pages/keto-flu/index.tsx
+++ pages/keto-flu/index.tsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import Head from 'next/head';
+import { symptoms } from '../../components/static_data/flu_symptoms';
 
 const KetoFlu: React.FC = (): JSX.Element => {
   return (
     <>
-      <h1 className="mx-5 text-[2rem] sm:text-[3.5rem] font-bold text-center">Come back later!</h1>
+      <Head>
+        <title>Keto Flu | Metabolite</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+
+      <section className="flex flex-col items-center mt-5 ">
+        <h2 className="font-bold text-md sm:text-2xl text-primary mt-5">The Keto Flu</h2>
+        <div className="w-[18rem] sm:w-[40rem] text-sm sm:text-xl">
+          <p className="mt-5">
+            In your first couple of weeks, you will likely experience something called keto flu.
+            Keto flu symptoms may include:
+          </p>
+          <ol className="mt-5 list">
+            {symptoms.map((sym: string) => {
+              return (
+                <li key={sym} className="list-disc">
+                  {sym}
+                </li>
+              );
+            })}
+          </ol>
+          <h3 className="mt-5 font-bold text-lg">Ultimately, calories in vs. calories out</h3>
+          <p className="mt-5">
+            Do not fret, however, as this is only temporary. This happens because your body is not
+            yet used to using fat as energy. Once you have been fully adapted to fat-energy, these
+            symptoms should go away. However, the symptoms may come back if you have been kicked out
+            of ketosis.
+          </p>
+        </div>
+      </section>
     </>
   );
 };
