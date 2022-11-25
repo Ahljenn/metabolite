313fa8a9a7ee776b5291bb9a8e9afac0bb44f3d8
Ahljenn <ahljennm2@gmail.com>
2022-11-24 21:12:29 -0800

    bottom margin on source

--- pages/sources/index.tsx
+++ pages/sources/index.tsx
@@ -9,7 +9,7 @@ const Sources: React.FC = (): JSX.Element => {
         <link rel="icon" href="/favicon.png" />
       </Head>
 
-      <section className="flex flex-col items-center mt-5 ">
+      <section className="flex flex-col items-center mt-5 mb-[10rem]">
         <h2 className="font-bold text-md sm:text-2xl text-primary mt-5">Sources</h2>
         <div className="w-[18rem] sm:w-[40rem] text-sm sm:text-xl">
           {citations.map((citation: string, idx) => {
