8b2882b5d510784286af56c0c3bec5cebdeb829a
Ahljenn <ahljennm2@gmail.com>
2022-11-24 20:59:42 -0800

    changed language for formula

--- components/features/BmrCalculator.tsx
+++ components/features/BmrCalculator.tsx
@@ -55,7 +55,7 @@ const BmrCalculator: React.FC = (): JSX.Element => {
           <div>
             <input type="radio" name="bmr-type" id="mifflin" value="mifflin"></input>
             <label htmlFor="mifflin" className="inline mx-2">
-              Mifflin St Jeor (Default)
+              Mifflin St Jeor (Recommended)
             </label>
           </div>
 
