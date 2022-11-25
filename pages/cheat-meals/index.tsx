a90621c316ba1464a3fffe68091f3f687f8b70ca
Ahljenn <ahljennm2@gmail.com>
2022-11-24 20:26:26 -0800

    cheat meals

--- pages/cheat-meals/index.tsx
+++ pages/cheat-meals/index.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import Head from 'next/head';
 
 const CheatMeals: React.FC = (): JSX.Element => {
   return (
     <>
-      <h1 className="mx-5 text-[2rem] sm:text-[3.5rem] font-bold text-center">Come back later!</h1>
+      <Head>
+        <title>Cheat Meals | Metabolite</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+
+      <section className="flex flex-col items-center mt-5 ">
+        <h2 className="font-bold text-md sm:text-2xl text-primary mt-5">Cheat Meals</h2>
+        <div className="w-[18rem] sm:w-[40rem] text-sm sm:text-xl">
+          <p className="mt-5">
+            It is highly not recommending having cheat meals during ketosis, even if it is only once
+            a week. Your body takes a while to get into ketosis, and if you cheat once (eat too many
+            carbs), your body will instantly shut down ketosis. Afterwards, you’ll have to build
+            your way back up to ketosis again, which can take a few days. That’s a few days of
+            progress lost because of one cheat meal.
+          </p>
+        </div>
+      </section>
     </>
   );
 };
