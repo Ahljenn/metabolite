4759ad7caf2c963b87eaeef4449505d29c76df34
Ahljenn <ahljennm2@gmail.com>
2023-06-26 14:58:14 -0700

    added about section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,7 +14,7 @@ interface teamCardProps {
 const teamCardData: teamCardProps[] = [
   {
     name: 'Ahljenn Mallari',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    desc: "Ahljenn is the founding software engineer with a profound passion for intersecting health and technology. He brings a strong foundation in full-stack development and a relentless drive to deliver innovative solutions that positively impact the world. Ahljenn's true calling lies in leveraging expertise to develop groundbreaking technologies that enhance people's lives in domains such as healthcare, education, and scientific advancements. Experience encompasses deploying robust web applications, harnessing the power of artificial intelligence, and working with operating systems. As the only developer of the team, Ahljenn's unwavering dedication and vision align seamlessly with our pursuit of integrating health and technology to transform lives.",
     image: '/aj.png',
     linkedin: 'https://www.linkedin.com/in/ahljenn-mallari-859026164',
     role: 'Founding Software Engineer',
@@ -22,7 +22,7 @@ const teamCardData: teamCardProps[] = [
   },
   {
     name: 'Dylan Sarmiento',
-    desc: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    desc: "Dylan is a dedicated researcher with a deep-rooted passion for nutrition and health. With a keen interest in understanding the intricate relationship between nutrition, metabolism, and overall well-being, he is driven to contribute to the field's advancement. Committed to supporting others on their path to success and optimal health, he endeavors to create a brighter and healthier future for all. Alongside his research pursuits, Dylan has honed his teaching skills through tutoring high school students in math, science, and English. While versatile in various subjects, his expertise shines in algebra and biology. Dylan is a valuable member of the two-man team, bringing both knowledge and a genuine passion for promoting nutrition and health.",
     image: '/dy.png',
     linkedin: 'https://www.linkedin.com/in/dylan-anthony-sarmiento-982a00225',
     role: 'Researcher',
@@ -35,7 +35,7 @@ export default function About() {
     <>
       <TopGradient fromColor={'to-green-400'} toColor={'from-teal-400'} />
       <h1 className="text-3xl font-sans italic">Meet the Team</h1>
-      <div className="flex gap-10 flex-col lg:flex-row">
+      <div className="mt-5 flex gap-10 flex-col lg:flex-row">
         {teamCardData.map((card: teamCardProps, index: number) => {
           return (
             <Link
