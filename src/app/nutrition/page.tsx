6e8678f7ee2d2a7fa4e5b6adf24d84eeb88009e7
Ahljenn <ahljennm2@gmail.com>
2023-06-26 12:49:59 -0700

    gradient component

--- src/app/nutrition/page.tsx
+++ src/app/nutrition/page.tsx
@@ -1,27 +1,27 @@
-import { TopGradient } from './components/ui/TopGradient';
 import Image from 'next/image';
+import { TopGradient } from '../components/ui/TopGradient';
 
-const rootCardData: { header: string; desc: string; route: string }[] = [
-  {
-    header: 'Fitness',
-    desc: 'Curate workouts that align perfectly with your goals, abilities, and preferences.',
-    route: '/fitness',
-  },
-  {
-    header: 'Nutrition',
-    desc: 'Discover the power of nutrition and its impact on your overall well-being.',
-    route: '/nutrition',
-  },
-  {
-    header: 'User Guide',
-    desc: 'Empower yourself with the knowledge and tools to make the most out of your experience.',
-    route: '/user-guide',
-  },
-  {
-    header: 'About',
-    desc: 'Learn more about our commitment to empower you on your wellness journey.',
-    route: '/about',
-  },
+const nutritionCardData: { header: string; desc: string; route: string }[] = [
+  // {
+  //   header: 'Fitness',
+  //   desc: 'Curate workouts that align perfectly with your goals, abilities, and preferences.',
+  //   route: '/fitness',
+  // },
+  // {
+  //   header: 'Nutrition',
+  //   desc: 'Discover the power of nutrition and its impact on your overall well-being.',
+  //   route: '/nutrition',
+  // },
+  // {
+  //   header: 'User Guide',
+  //   desc: 'Empower yourself with the knowledge and tools to make the most out of your experience.',
+  //   route: '/user-guide',
+  // },
+  // {
+  //   header: 'About',
+  //   desc: 'Learn more about our commitment to empower you on your wellness journey.',
+  //   route: '/about',
+  // },
 ];
 
 export default function Home() {
@@ -49,21 +49,11 @@ export default function Home() {
         </div>
       </div>
 
-      <TopGradient fromColor={'from-green-200'} toColor={'to-emerald-800'} />
+      <TopGradient fromColor={'to-emerald-800'} toColor={'from-green-200'} />
 
-      <Image
-        // className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-        className="relative "
-        src="/metabolite-full.png"
-        alt="Metabolite logo"
-        width={200}
-        height={200}
-        quality={100}
-        priority
-      />
-
+      {/* 
       <div className="mt-20 mb-32 lg:mt-0 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        {rootCardData.map(
+        {nutritionCardData.map(
           (card: { header: string; desc: string; route: string }, index: number) => {
             return (
               <a
@@ -83,7 +73,7 @@ export default function Home() {
             );
           }
         )}
-      </div>
+      </div> */}
     </main>
   );
 }
