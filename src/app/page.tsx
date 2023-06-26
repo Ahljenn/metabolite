dff2dde413655ec5bd14dc2d9eb04d959f9bfd31
Ahljenn <ahljennm2@gmail.com>
2023-06-26 11:48:45 -0700

    changed background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,17 +48,17 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-emerald-200 after:via-green-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-green-700 before:dark:opacity-10 after:dark:from-emerald-900 after:dark:via-green-600 after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          // className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          className="relative "
-          src="/metabolite-full.png"
-          alt="Metabolite logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div className="absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-green-200 to-emerald-800 rounded-md filter blur-3xl opacity-50 -z-50" />
+
+      <Image
+        // className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+        className="relative "
+        src="/metabolite-full.png"
+        alt="Metabolite logo"
+        width={180}
+        height={37}
+        priority
+      />
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         {rootCardData.map(
