397dcc20569abbce4f27718820fa358e9153bf65
Ahljenn <ahljennm2@gmail.com>
2023-06-26 11:41:18 -0700

    added routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,25 @@
 import Image from 'next/image';
 
-const rootCardData: { header: string; desc: string }[] = [
+const rootCardData: { header: string; desc: string; route: string }[] = [
   {
     header: 'Fitness',
     desc: 'Curate workouts that align perfectly with your goals, abilities, and preferences.',
+    route: '/fitness',
   },
   {
     header: 'Nutrition',
     desc: 'Discover the power of nutrition and its impact on your overall well-being.',
+    route: '/nutrition',
   },
   {
     header: 'User Guide',
     desc: 'Empower yourself with the knowledge and tools to make the most out of your experience.',
+    route: '/user-guide',
   },
   {
     header: 'About',
     desc: 'Learn more about our commitment to empower you on your wellness journey.',
+    route: '/about',
   },
 ];
 
@@ -57,25 +61,27 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        {rootCardData.map((card: { header: string; desc: string }, index: number) => {
-          return (
-            <a
-              key={index}
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h2 className={`mb-3 text-2xl font-semibold`}>
-                {card.header}
-                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                  -&gt;
-                </span>
-              </h2>
-              <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>{card.desc}</p>
-            </a>
-          );
-        })}
+        {rootCardData.map(
+          (card: { header: string; desc: string; route: string }, index: number) => {
+            return (
+              <a
+                key={index}
+                href={card.route}
+                className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h2 className={`mb-3 text-2xl font-semibold`}>
+                  {card.header}
+                  <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                    -&gt;
+                  </span>
+                </h2>
+                <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>{card.desc}</p>
+              </a>
+            );
+          }
+        )}
       </div>
     </main>
   );
