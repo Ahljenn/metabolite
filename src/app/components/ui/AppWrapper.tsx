f7075086df9f767bf88c4930b4532f44d6de3620
Ahljenn <ahljennm2@gmail.com>
2023-06-27 13:22:15 -0700

    page animations

--- src/app/components/ui/AppWrapper.tsx
+++ src/app/components/ui/AppWrapper.tsx
@@ -3,6 +3,13 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
+import { motion } from 'framer-motion';
+
+const variants = {
+  pageInitial: { opacity: 0 },
+  pageAnimate: { opacity: 1 },
+};
+
 interface HeaderProps {
   children: any;
 }
@@ -10,40 +17,48 @@ interface HeaderProps {
 export default function Header({ children }: HeaderProps) {
   const pathname = usePathname();
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center ${
-        pathname === '/' ? 'justify-between' : ''
-      } p-24`}
+    <motion.div
+      key={pathname}
+      initial="pageInitial"
+      transition={{ duration: 1 }}
+      animate="pageAnimate"
+      variants={variants}
     >
-      <header className="z-10 w-full max-w-5xl items-center justify-between text-sm lg:flex">
-        <div className="flex flex-col items-center lg:items-stretch gap-5 lg:flex-row lg:flex-end">
-          <HomeIcon />
-          <div>
-            <h1 className="text-center mb-10 text-lg lg:mb-0 ">Metabolite</h1>
+      <main
+        className={`flex min-h-screen flex-col items-center ${
+          pathname === '/' ? 'justify-between' : ''
+        } p-24`}
+      >
+        <header className="z-10 w-full max-w-5xl items-center justify-between text-sm lg:flex">
+          <div className="flex flex-col items-center lg:items-stretch gap-5 lg:flex-row lg:flex-end">
+            <HomeIcon />
+            <div>
+              <h1 className="text-center mb-10 text-lg lg:mb-0 ">Metabolite</h1>
+            </div>
           </div>
-        </div>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none hover:animate-pulse">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://github.com/Ahljenn/metabolite"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/github.png"
-              alt="Github Logo"
-              draggable="false"
-              className="dark:invert"
-              width={20}
-              height={20}
-              priority
-            />
-            Contribute
-          </a>
-        </div>
-      </header>
-      {children}
-    </main>
+          <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none hover:animate-pulse">
+            <a
+              className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+              href="https://github.com/Ahljenn/metabolite"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/github.png"
+                alt="Github Logo"
+                draggable="false"
+                className="dark:invert"
+                width={20}
+                height={20}
+                priority
+              />
+              Contribute
+            </a>
+          </div>
+        </header>
+        {children}
+      </main>
+    </motion.div>
   );
 }
 
