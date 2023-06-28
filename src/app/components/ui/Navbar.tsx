071dcab88035115e3b0adfdfa74f44e16e3543e5
Ahljenn <ahljennm2@gmail.com>
2023-06-28 14:12:22 -0700

    added cards

--- src/app/components/ui/Navbar.tsx
+++ src/app/components/ui/Navbar.tsx
@@ -65,7 +65,7 @@ export default function Navbar({ currentRoute }: NavBarProps) {
                         className={classNames(
                           item.href === currentRoute
                             ? 'bg-gray-900 text-white'
-                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                            : 'text-gray-300 hover:bg-gray-900 hover:text-white',
                           'rounded-md px-3 py-2 text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
--- src/app/nutrition/page.tsx
+++ src/app/nutrition/page.tsx
@@ -2,56 +2,65 @@ import Image from 'next/image';
 import { TopGradient } from '../components/ui/TopGradient';
 
 const nutritionCardData: { header: string; desc: string; route: string }[] = [
-  // {
-  //   header: 'Fitness',
-  //   desc: 'Curate workouts that align perfectly with your goals, abilities, and preferences.',
-  //   route: '/fitness',
-  // },
-  // {
-  //   header: 'Nutrition',
-  //   desc: 'Discover the power of nutrition and its impact on your overall well-being.',
-  //   route: '/nutrition',
-  // },
-  // {
-  //   header: 'User Guide',
-  //   desc: 'Empower yourself with the knowledge and tools to make the most out of your experience.',
-  //   route: '/user-guide',
-  // },
-  // {
-  //   header: 'About',
-  //   desc: 'Learn more about our commitment to empower you on your wellness journey.',
-  //   route: '/about',
-  // },
+  {
+    header: 'Diet',
+    desc: 'The specific food and drinks a person consumes regularly to meet their nutritional needs and achieve their health and fitness goals. The goal of a diet is often to achieve specific health and fitness objectives, such as weight management, muscle gain, improved athletic performance, or the prevention of chronic diseases. Different dietary approaches, such as vegetarian, vegan, Mediterranean, or low-carb diets, exist to cater to individual needs and preferences.',
+    route: '#',
+  },
+  {
+    header: 'Basal Metaboic Rate',
+    desc: 'Learn more about the amount of energy expended by your body at rest to maintain basic bodily functions such as breathing, circulation, and cell production.',
+    route: '#',
+  },
+  {
+    header: 'Nutrients',
+    desc: 'The essential components of food that are vital for optimal fitness and health.',
+    route: '#',
+  },
+
+  {
+    header: 'Water',
+    desc: 'A vital liquid essential for maintaining hydration, regulating body temperature, transporting nutrients, and eliminating waste from the body.',
+    route: '#',
+  },
+  {
+    header: 'Fasting',
+    desc: 'Often practiced for various health and spiritual reasons, or as a strategy for weight management and metabolic health.',
+    route: '#',
+  },
 ];
 
-export default function Home() {
+export default function Nutrition() {
   return (
     <>
+      <h1 className="text-4xl lg:text-6xl font-bold text-center mt-5">Metabolite Nutrition</h1>
       <TopGradient fromColor={'to-emerald-400'} toColor={'from-green-400'} />
 
-      {/* 
-      <div className="mt-20 mb-32 lg:mt-0 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="mt-20 grid text-center lg:mb-0 lg:grid-cols-3 lg:text-left gap-5">
         {nutritionCardData.map(
           (card: { header: string; desc: string; route: string }, index: number) => {
             return (
               <a
                 key={index}
                 href={card.route}
-                className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                className={`${
+                  card.header === 'Diet' ? 'col-span-2' : ''
+                } rounded-lg  border border-transparent px-5 py-4 border-neutral-800 bg-neutral-900 hover:border-neutral-700 hover:bg-neutral-800 transition-all`}
                 rel="noopener noreferrer"
               >
-                <h2 className={`mb-3 text-2xl font-semibold`}>
-                  {card.header}
-                  <span className="inline-block transition-transform group-hover:translate-x-2 motion-reduce:transform-none">
-                    -&gt;
-                  </span>
-                </h2>
-                <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>{card.desc}</p>
+                <h2 className={`mb-3 text-2xl font-semibold`}>{card.header}</h2>
+                <p
+                  className={`m-0 ${
+                    card.header === 'Diet' ? 'max-w-[70ch]' : 'max-w-[30ch]'
+                  } text-sm opacity-50`}
+                >
+                  {card.desc}
+                </p>
               </a>
             );
           }
         )}
-      </div> */}
+      </div>
     </>
   );
 }
