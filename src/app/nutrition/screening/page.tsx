e6ec9b3dfcadc8f8d76f647a31ffcbf3cf935d78
Ahljenn <ahljennm2@gmail.com>
2023-07-11 19:01:29 -0700

    fixed to make global darkmode visible

--- src/app/nutrition/screening/page.tsx
+++ src/app/nutrition/screening/page.tsx
@@ -308,19 +308,17 @@ export default function Screening() {
       <h1 className="text-4xl lg:text-6xl font-bold text-center mt-5">Metabolite Nutrition</h1>
       <div
         className="z-[-100] relative flex place-items-center before:absolute before:h-[800px] before:w-[480px] 
-      before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent 
-      before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[280px] after:w-[240px] 
-      after:bg-gradient-conic after:from-emerald-200 after:via-green-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br 
-      before:dark:from-transparent before:dark:to-green-800 before:dark:opacity-10 after:dark:from-emerald-900 after:dark:via-green-800 
-      after:dark:opacity-40 before:lg:h-[260px] translate-x-[-25rem] translate-y-[12rem] "
+      before:rounded-full before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[280px] after:w-[240px] 
+      after:bg-gradient-conic after:blur-2xl after:content-[''] before:bg-gradient-to-br 
+      before:from-transparent before:to-green-800 before:opacity-10 after:from-emerald-800 after:via-lime-800 
+      after:opacity-40 before:lg:h-[260px] translate-x-[-25rem] translate-y-[12rem] "
       />
       <div
         className="z-[-100] relative flex place-items-center before:absolute before:h-[800px] before:w-[480px] 
-      before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent 
-      before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[380px] after:w-[340px] 
-      after:bg-gradient-conic after:from-emerald-200 after:via-green-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br 
-      before:dark:from-transparent before:dark:to-green-700 before:dark:opacity-10 after:dark:from-emerald-900 after:dark:via-green-600 
-      after:dark:opacity-40 before:lg:h-[460px] translate-y-[30rem]"
+      before:rounded-full before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[380px] after:w-[340px] 
+      after:bg-gradient-conic  after:blur-2xl after:content-[''] before:bg-gradient-to-br 
+      before:from-transparent before:to-green-700 before:opacity-10 after:from-emerald-900 after:via-green-600 
+      after:opacity-40 before:lg:h-[460px] translate-y-[30rem]"
       />
       {!isScreeningComplete ? (
         <>
