49896d3088e983855f60ef03e676eb4a3aa09894
Ahljenn <aamallari@ucdavis.edu>
2023-08-06 17:27:45 -0700

    fixed type for providers

--- src/app/global-redux/provider.tsx
+++ src/app/global-redux/provider.tsx
@@ -3,6 +3,10 @@
 import { Provider } from 'react-redux';
 import { store } from './store';
 
-export function Providers({ children }) {
+interface AppProps {
+  children: any;
+}
+
+export function Providers({ children }: AppProps) {
   return <Provider store={store}>{children}</Provider>;
 }
