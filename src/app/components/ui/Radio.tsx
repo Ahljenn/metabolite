692e20a29d187c8bb15fcb3ffd150a20794f7c1f
Ahljenn <ahljennm2@gmail.com>
2023-08-08 13:27:20 -0700

    check circle color change

--- src/app/components/ui/Radio.tsx
+++ src/app/components/ui/Radio.tsx
@@ -82,7 +82,7 @@ const Radio = ({ items, setSelection, label, existingSelection, isRow }: RadioPr
 function CheckIcon(props: any) {
   return (
     <svg viewBox="0 0 24 24" fill="none" {...props}>
-      <circle cx={12} cy={12} r={12} fill="#fff" opacity="0.2" />
+      <circle cx={12} cy={12} r={12} fill="#00B287" opacity="0.2" />
       <path
         d="M7 13l3 3 7-7"
         stroke="#fff"
