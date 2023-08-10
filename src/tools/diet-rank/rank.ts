6a752d87985f4715e6610b6232aff1e5402ac78a
Ahljenn <ahljennm2@gmail.com>
2023-08-10 07:19:30 -0700

    factored dietary preferences

--- src/tools/diet-rank/rank.ts
+++ src/tools/diet-rank/rank.ts
@@ -20,41 +20,40 @@ let clusterScore: { [key: string]: number } = {
 
 export const rank = (metrics: UserScreeningType, normalizeScore: boolean = false) => {
   const bmr = calculateBmr(metrics);
-
   const normalizedBmr = bmr / 1000;
 
   // Diminishing factor based on BMR value for KTO, VEG, FLX
   // The higher the BMR, the greater caloric needs will be for certain diets
-  clusterScore['KTO'] += 1 / Math.log(normalizedBmr + 2);
+  clusterScore['KTO'] += 1 / Math.log(normalizedBmr);
   clusterScore['HPO'] += Math.sqrt(normalizedBmr);
-  clusterScore['VEG'] += 1 / Math.log(normalizedBmr + 2);
-  clusterScore['FLX'] += 1 / Math.log(normalizedBmr + 2);
+  clusterScore['VEG'] += 1 / Math.log(normalizedBmr);
+  clusterScore['FLX'] += 1 / Math.log(normalizedBmr);
   clusterScore['ZON'] += Math.sqrt(normalizedBmr);
 
   // Health Goals
   const goal = metrics.healthGoal;
   if (goal === 'Building Muscle' || goal === 'Building Strength') {
-    clusterScore['HPO'] += 4;
-    clusterScore['KTO'] += 1;
-    clusterScore['VEG'] -= 2;
+    clusterScore['HPO'] += 3;
+    clusterScore['KTO'] += 0.5;
+    clusterScore['VEG'] -= 1;
   } else if (
     goal === 'Losing Weight' ||
     goal === 'Enhancing Cardiovascular Health' ||
     goal === 'Managing Diabetes'
   ) {
-    clusterScore['ZON'] += 4;
-    clusterScore['KTO'] += 2;
-    clusterScore['VEG'] += 1;
-  } else if (goal === 'Performance Enhancement' || goal === 'Body Recomposition') {
-    clusterScore['HPO'] += 4;
-    clusterScore['FLX'] += 2;
-  } else if (goal === 'Ethical or Environment Concerns') {
-    clusterScore['VEG'] += 8;
-    clusterScore['FLX'] += 4;
-    clusterScore['HPO'] -= 2;
-  } else if (goal === 'Gut Health') {
-    clusterScore['FLX'] += 4;
+    clusterScore['ZON'] += 3;
     clusterScore['KTO'] += 1;
+    clusterScore['VEG'] += 0.5;
+  } else if (goal === 'Performance Enhancement' || goal === 'Body Recomposition') {
+    clusterScore['HPO'] += 3;
+    clusterScore['FLX'] += 1;
+  } else if (goal === 'Ethical or Environment Concerns') {
+    clusterScore['VEG'] += 5;
+    clusterScore['FLX'] += 3;
+    clusterScore['HPO'] -= 1;
+  } else if (goal === 'Gut Health') {
+    clusterScore['FLX'] += 2;
+    clusterScore['KTO'] += 0.5;
   }
 
   // Compute on activity and work multipliers
@@ -66,9 +65,18 @@ export const rank = (metrics: UserScreeningType, normalizeScore: boolean = false
   clusterScore['FLX'] += (activityMultiplier[activityLevel] + workMultiplier[workExertion]) * 1.35;
   clusterScore['ZON'] += (activityMultiplier[activityLevel] + workMultiplier[workExertion]) * 1.25;
 
-  // TODO: Existing dietary preferences:
-
-  // ----------------------------------------
+  // Existing dietary preferences
+  const pref = metrics.dietPref;
+  if (pref === 'Ketogenic Diet') {
+    clusterScore['KTO'] += 5;
+  } else if (pref === 'Zone Diet') {
+    clusterScore['ZON'] += 5;
+  } else if (pref === 'Vegan Diet') {
+    clusterScore['VEG'] += 5;
+    clusterScore['FLX'] += 2.5;
+  } else if (pref === 'Paleo Diet') {
+    clusterScore['HPO'] += 5;
+  }
 
   // Normalize scores to between 0 - 1
   if (normalizeScore) {
