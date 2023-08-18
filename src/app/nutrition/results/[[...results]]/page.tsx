fd495cfb3bde540ae8d5f3208b60d3f44fc72440
Ahljenn <aamallari@ucdavis.edu>
2023-08-18 14:19:56 -0700

    added dynamic/optional routing

--- src/app/nutrition/results/[[...results]]/page.tsx
+++ src/app/nutrition/results/[[...results]]/page.tsx
@@ -3,12 +3,14 @@ import type { RootState } from '&/store';
 import { useSelector, useDispatch } from 'react-redux';
 // import { increment, decrement, incrementByAmount } from '&/features/user-metrics/metricsSlice';
 
-const Results: React.FC = (): JSX.Element => {
+const Results = ({ params }: { params: { results: string } }): JSX.Element => {
   // const countState = useSelector((state: RootState) => state.metrics.value);
+
+  const { results } = params;
 
   return (
     <div className="mt-5 mx-auto w-full max-w-md lg:max-w-xl">
-      state:
+      <h2>test: {results}</h2>
       {/* {countState} */}
     </div>
   );
