bfb78135989f3a90e6b0e680315a89f368f0c37b
Ahljenn <aamallari@ucdavis.edu>
2023-08-18 14:24:32 -0700

    added no results section

--- src/app/components/ui/UnderConstruction.tsx
+++ src/app/components/ui/UnderConstruction.tsx
@@ -8,13 +8,13 @@ const UnderConstruction = () => {
         Exciting things are happening behind the scenes! This page is undergoing a transformation to
         bring you an even more incredible experience. Stay tuned for exciting updates.
       </p>
-      <Link
+      <a
         className="mt-5 border-neutral-800 bg-neutral-900 hover:border-neutral-700 hover:bg-neutral-800 transition-all border rounded-lg py-2 px-4 whitespace-nowrap"
         href="/"
         rel="noopener noreferrer"
       >
         Return to Hub
-      </Link>
+      </a>
     </>
   );
 };
--- src/app/nutrition/results/[[...results]]/page.tsx
+++ src/app/nutrition/results/[[...results]]/page.tsx
@@ -8,6 +8,27 @@ const Results = ({ params }: { params: { results: string } }): JSX.Element => {
 
   const { results } = params;
 
+  if (!results) {
+    return (
+      <div className="mt-5 mx-auto w-full max-w-md lg:max-w-xl flex flex-col items-center">
+        <h1 className="text-4xl lg:text-6xl font-bold text-center mt-5">No results</h1>
+        <p className="mt-5 text-center opacity-75 mx-10 w-3/4">
+          Looks like you have no screening results! Let&apos;s navigate back to the nutrition page
+          to get started.
+        </p>
+        <div className="mt-5 ">
+          <a
+            className="mt-5 border-neutral-800 bg-neutral-900 hover:border-neutral-700 hover:bg-neutral-800 transition-all border rounded-lg py-2 px-4 whitespace-nowrap"
+            href="/nutrition"
+            rel="noopener noreferrer"
+          >
+            Return to Nutrition
+          </a>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="mt-5 mx-auto w-full max-w-md lg:max-w-xl">
       <h2>test: {results}</h2>
