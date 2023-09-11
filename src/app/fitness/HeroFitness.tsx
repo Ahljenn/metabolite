d082421461338a9e745c53c43b4007450d052db0
Ahljenn <aamallari@ucdavis.edu>
2023-09-10 17:28:49 -0700

    changed header text

--- src/app/fitness/HeroFitness.tsx
+++ src/app/fitness/HeroFitness.tsx
@@ -19,27 +19,27 @@ const HeroFitness = () => {
       <div className="flex max-w-screen-xl px-4 py-8 mx-auto flex-col sm:flex-row">
         <div className="mr-auto place-self-center text-center sm:text-left">
           <h1 className="max-w-2xl mb-4 text-xl sm:text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white m-5 sm:m-0">
-            Metabolite Fitness
+            Unlock Your Potential
           </h1>
           <p className="max-w-2xl mb-6 sm:mb-8 md:text-lg sm:text-xl text-white m-5 sm:m-0">
             Welcome to Metabolite: Fitness. Customize your strength training with personalized
             workout plans and elevate your fitness journey to new heights.
           </p>
           <div className="flex flex-col-reverse sm:flex-row gap-8 justify-center sm:justify-start items-center">
-            <div>
-              <a
-                href="/fitness/screening"
-                className="mt-5 border-blue-300 bg-cyan-700 hover:border-blue-200 hover:bg-cyan-600 transition-all border rounded-lg py-2 px-4 whitespace-nowrap"
-              >
-                Get Started
-              </a>
-            </div>
-            <div>
+            {/* <div>
               <a
                 href="/about"
                 className="mt-5 border-neutral-800 bg-neutral-900 hover:border-neutral-700 hover:bg-neutral-800 transition-all border rounded-lg py-2 px-4 whitespace-nowrap"
               >
                 About
+              </a>
+            </div> */}
+            <div>
+              <a
+                href="/fitness/screening"
+                className="mt-5 border-blue-300 bg-cyan-700 hover:border-blue-200 hover:bg-cyan-600 transition-all border rounded-lg py-2 px-4 whitespace-nowrap"
+              >
+                Get started - It&apos;s free
               </a>
             </div>
           </div>
