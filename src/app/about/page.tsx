d00feb278c85a93954d90ae09684dea79dbef743
Ahljenn <aamallari@ucdavis.edu>
2023-09-28 10:55:14 -0700

    removed images for about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,8 +4,6 @@ import Link from 'next/link';
 interface teamCardProps {
   name: string;
   desc: string;
-  image: string;
-  linkedin: string;
   role: string;
   education: string;
 }
@@ -19,16 +17,12 @@ const teamCardData: teamCardProps[] = [
   {
     name: 'Ahljenn Mallari',
     desc: "Ahljenn is the founding software engineer with a profound passion for intersecting health and technology. He brings a strong foundation in full-stack development and a relentless drive to deliver innovative solutions that positively impact the world. Ahljenn's true calling lies in leveraging expertise to develop groundbreaking technologies that enhance people's lives in domains such as healthcare, education, and scientific advancements. Experience encompasses deploying robust web applications, harnessing the power of artificial intelligence, and working with operating systems. As the only developer of the team, Ahljenn's unwavering dedication and vision align seamlessly with our pursuit of integrating health and technology to transform lives.",
-    image: '/aj.png',
-    linkedin: 'https://www.linkedin.com/in/ahljenn-mallari-859026164',
     role: 'Founding Software Engineer',
     education: 'BS in Computer Science',
   },
   {
     name: 'Dylan Sarmiento',
     desc: "Dylan is a dedicated researcher with a deep-rooted passion for nutrition and health. With a keen interest in understanding the intricate relationship between nutrition, metabolism, and overall well-being, he is driven to contribute to the field's advancement. Committed to supporting others on their path to success and optimal health, he endeavors to create a brighter and healthier future for all. Alongside his research pursuits, Dylan has honed his teaching skills through tutoring high school students in math, science, and English. While versatile in various subjects, his expertise shines in algebra and biology. Dylan is a valuable member of the two-man team, bringing both knowledge and a genuine passion for promoting nutrition and health.",
-    image: '/dy.png',
-    linkedin: 'https://www.linkedin.com/in/dylan-anthony-sarmiento-982a00225',
     role: 'Researcher',
     education: 'BS in Neurobiology, Physiology and Behavior',
   },
@@ -50,26 +44,16 @@ const About = () => {
       <div className="mt-5 flex gap-10 flex-col lg:flex-row">
         {teamCardData.map((card: teamCardProps, index: number) => {
           return (
-            <Link
+            <div
               key={index}
-              href={card.linkedin}
-              className="rounded-lg flex flex-col items-center border border-transparent p-5 hover:lg:border-neutral-700 hover:lg:bg-neutral-800/30"
+              className="rounded-lg flex flex-col items-center border border-transparent p-5 "
               rel="noopener noreferrer"
-              target="_blank"
             >
-              <Image
-                src={card.image}
-                alt="Team member"
-                width="250"
-                height="250"
-                className="rounded-full"
-                quality={100}
-              />
               <h2 className={`lg:text-2xl font-semibold mt-3 whitespace-nowrap`}>{card.name}</h2>
               <p className="text-sm opacity-75 ">{card.education}</p>
               <p className="mt-5 italic whitespace-nowrap">{card.role}</p>
               <p className={`m-0 max-w-[20rem] text-sm opacity-50`}>{card.desc}</p>
-            </Link>
+            </div>
           );
         })}
       </div>
