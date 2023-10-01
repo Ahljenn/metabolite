194f1addb1780e83ed6f63948841d72667106909
Ahljenn <aamallari@ucdavis.edu>
2023-10-01 16:39:10 -0700

    uninstall unneeded dependencies, removed redux

--- src/app/components/ui/AppWrapper.tsx
+++ src/app/components/ui/AppWrapper.tsx
@@ -1,6 +1,4 @@
 'use client';
-import ReduxProvider from '&/ReduxProvider';
-// import FadeWrapper from './FadeWrapper';
 import Footer from './Footer';
 
 interface AppWrapperProps {
@@ -9,12 +7,10 @@ interface AppWrapperProps {
 
 const AppWrapper = ({ children }: AppWrapperProps) => {
   return (
-    <ReduxProvider>
-      <div className="flex flex-col justify-between min-h-screen">
-        <section className="mb-20">{children}</section>
-        <Footer />
-      </div>
-    </ReduxProvider>
+    <div className="flex flex-col justify-between min-h-screen">
+      <section className="mb-20">{children}</section>
+      <Footer />
+    </div>
   );
 };
 export default AppWrapper;
