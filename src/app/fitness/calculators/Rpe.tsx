5189f19bb717170b2ca5bd8c4c6847df49073c36
Ahljenn <aamallari@ucdavis.edu>
2023-10-06 15:12:16 -0700

    added rpe scale

--- src/app/fitness/calculators/Rpe.tsx
+++ src/app/fitness/calculators/Rpe.tsx
@@ -1,6 +1,27 @@
 'use client';
 import { useState } from 'react';
 
+const rpeScale: { rpe: string; title: string; description: string }[] = [
+  { rpe: '1', title: 'Minimal Effort', description: 'Barely any exertion required' },
+  { rpe: '2-3', title: 'Warm-up', description: 'Very light, like a warm-up set' },
+  { rpe: '4-5', title: 'Working Set', description: 'Noticeable effort, but manageable' },
+  {
+    rpe: '6-7',
+    title: 'Moderate Effort',
+    description: 'Moderate effort, starting to feel challenging',
+  },
+  {
+    rpe: '8-9',
+    title: 'Demanding Effort',
+    description: 'Very challenging, close to maximum effort',
+  },
+  {
+    rpe: '10',
+    title: 'Maximum Effort',
+    description: "Absolute maximum effort, can't do more reps",
+  },
+];
+
 const Rpe = () => {
   const [weight, setWeight] = useState<number | null>(null);
   const [reps, setReps] = useState<number | null>(null);
@@ -18,6 +39,48 @@ const Rpe = () => {
           (RPE) is a subjective scale used to measure the intensity of physical activity. It&apos;s
           commonly used in sports and exercise science to gauge how hard an individual feels they
           are working during exercise.
+        </div>
+
+        <div className="relative overflow-x-auto mt-10 rounded-lg">
+          <div className="flex flex-col sm:flex-row sm:gap-5 sm:items-end">
+            <p className="text-xl font-bold">How do I measure RPE?</p>
+            <p className="lead-xl font-light opacity-75 max-w-2xl text-md">
+              See below for a guideline:
+            </p>
+          </div>
+          <table className="mt-5 w-full text-sm text-left  text-white">
+            <thead className="bg-neutral-800 text-white">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  RPE
+                </th>
+
+                <th scope="col" className="px-6 py-3">
+                  Intensity Level
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {rpeScale.map(
+                (item: { rpe: string; title: string; description: string }, idx: number) => {
+                  return (
+                    <tr className=" border-b bg-neutral-900 border-neutral-800" key={idx}>
+                      <th
+                        scope="row"
+                        className="px-6 py-4 font-medium text-2xl  whitespace-nowrap text-white"
+                      >
+                        {item.rpe}
+                      </th>
+                      <td className="px-6 py-4">
+                        <p className="font-medium">{item.title}</p>
+                        <p className="opacity-75 italic">{item.description}.</p>
+                      </td>
+                    </tr>
+                  );
+                }
+              )}
+            </tbody>
+          </table>
         </div>
       </div>
 
@@ -89,6 +152,7 @@ const Rpe = () => {
           />
         </div>
       </div>
+
       <div className="mt-5 flex justify-end flex-col sm:flex-row items-center gap-10">
         <button
           className={`transition-all border rounded-lg py-2 px-4 whitespace-nowrap border-metaPrimary bg-neutral-900 ${
