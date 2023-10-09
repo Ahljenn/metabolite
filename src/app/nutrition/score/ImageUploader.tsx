7c011b61d1da55e8d877e612d6d2aafe563e0dce
Ahljenn <aamallari@ucdavis.edu>
2023-10-09 11:49:33 -0700

    working optical character recognition

--- src/app/nutrition/score/ImageUploader.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import { useState } from 'react';
-
-const ImageUploader = () => {
-  return <div>ImageUploader</div>;
-};
-
-export default ImageUploader;
--- src/app/nutrition/score/OpticalCharacterRecognition.tsx
+++ src/app/nutrition/score/OpticalCharacterRecognition.tsx
@@ -1,12 +1,22 @@
 'use client';
 import { useState } from 'react';
 import Tesseract from 'tesseract.js';
+import ImageUploader from '../../components/features/ImageUploader';
 
 const OpticalCharacterRecognition = () => {
-  const [image, setImage] = useState(null);
+  const [extractedText, setExtractedText] = useState<string>('');
+
+  const extractText = async (file: File) => {
+    const res = await Tesseract.recognize(file, 'eng', { logger: (info) => console.log(info) });
+    setExtractedText(res.data.text);
+  };
+
   return (
     <section>
-      <div className="py-8 flex flex-col sm:items-left gap-5 justify-center"></div>
+      <div className="py-8 flex flex-col sm:items-left gap-5 justify-center">
+        <ImageUploader callback={extractText} />
+        Text: {extractedText}
+      </div>
     </section>
   );
 };
