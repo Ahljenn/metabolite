065ad559a757a5d20460e7bd0c6daf9ba4750d4c
Ahljenn <aamallari@ucdavis.edu>
2023-10-11 13:38:54 -0700

    added object containing the scores for items

--- src/app/api/ml-service/route.ts
+++ src/app/api/ml-service/route.ts
@@ -1,75 +1,82 @@
 import { NextResponse } from 'next/server';
 
 const extractFeatures = (text: string) => {
-  // Food Additives to Avoid
   const foodAdditives = [
-    'SODIUM NITRATE',
-    'SULFITES',
-    'AZODICARBONAMIDE',
-    'POTASSIUM BROMATE',
-    'PROPYL GALLATE',
-    'BHA',
-    'BHT',
-    'PROPYLENE GLYCOL',
-    'BUTANE',
-    'MONOSODIUM GLUTAMATE',
-    'DISODIUM INOSINATE',
-    'DISODIUM GUANYLATE',
-    'ENRICHED FLOUR',
-    'RECOMBINANT BOVINE GROWTH HORMONE (RBGH)',
-    'REFINED VEGETABLE OIL',
-    'SODIUM BENZOATE',
-    'BROMINATED VEGETABLE OIL',
-    'OLESTRA',
-    'CARRAGEENAN',
-    'POLYSORBATE 60',
-    'CARNAUBA WAX',
-    'MAGNESIUM SULPHATE',
-    'CHLORINE DIOXIDE',
-    'PARABEN',
-    'SODIUM CARBOXYMETHYL CELLULOSE',
-    'ALUMINUM',
+    { name: 'SODIUM NITRATE', score: 'WORST OFFENDER' },
+    { name: 'SULFITES', score: 'MODERATE' },
+    { name: 'AZODICARBONAMIDE', score: 'MODERATE' },
+    { name: 'POTASSIUM BROMATE', score: 'WORST OFFENDER' },
+    { name: 'PROPYL GALLATE', score: 'MODERATE' },
+    { name: 'BHA/BHT', score: 'WORST OFFENDER' },
+    { name: 'PROPYLENE GLYCOL', score: 'GENERALLY SAFE (FDA)' },
+    { name: 'BUTANE', score: 'KNOWN CARCINOGEN' },
+    { name: 'MONOSODIUM GLUTAMATE (MSG)', score: 'LINKED TO HEALTH ISSUES' },
+    { name: 'DISODIUM INOSINATE', score: 'CONTAINS MSG' },
+    { name: 'DISODIUM GUANYLATE', score: 'CONTAINS MSG' },
+    { name: 'ENRICHED FLOUR', score: 'MADE FROM TOXIC INGREDIENTS' },
+    { name: 'RECOMBINANT BOVINE GROWTH HORMONE (RBGH)', score: 'LINKED TO CANCER' },
+    { name: 'REFINED VEGETABLE OIL', score: 'LINKED TO HEALTH ISSUES' },
+    { name: 'SODIUM BENZOATE', score: 'KNOWN CARCINOGEN' },
+    { name: 'BROMINATED VEGETABLE OIL', score: 'POISON, CAUSES ORGAN DAMAGE' },
+    { name: 'PROPYL GALLATE', score: 'LINKED TO CANCER' },
+    { name: 'OLESTRA', score: 'DIGESTIVE PROBLEMS, UNHEALTHY FOR HEART' },
+    { name: 'CARRAGEENAN', score: 'CAN CAUSE ULCERS AND CANCER' },
+    { name: 'POLYSORBATE 60', score: 'CAN CAUSE CANCER' },
+    { name: 'CAMAUBA WAX', score: 'CAN CAUSE CANCER AND TUMORS' },
+    { name: 'MAGNESIUM SULPHATE', score: 'CAN CAUSE CANCER' },
+    { name: 'CHLORINE DIOXIDE', score: 'CAN CAUSE TUMORS AND HYPERACTIVITY' },
+    { name: 'PARABEN', score: 'DISRUPTS HORMONES, LINKED TO CANCER' },
+    { name: 'SODIUM CARBOXYMETHYL CELLULOSE', score: 'MAY CAUSE CANCER' },
+    { name: 'ALUMINUM', score: 'CAN CAUSE CANCER' },
   ];
 
-  // Artificial Sweeteners to Avoid
   const artificialSweeteners = [
-    'SACCHARIN',
-    'ASPARTAME',
-    'HIGH FRUCTOSE CORN SYRUP',
-    'ACESULFAME POTASSIUM',
-    'SUCRALOSE',
-    'AGAVE NECTAR',
-    'BLEACHED STARCH',
-    'TERT BUTYLHYDROQUINONE',
+    { name: 'SACCHARIN', score: 'KNOWN CARCINOGEN' },
+    { name: 'ASPARTAME', score: 'POTENTIAL CARCINOGEN' },
+    { name: 'HIGH FRUCTOSE CORN SYRUP', score: 'LINKED TO VARIOUS HEALTH ISSUES' },
+    { name: 'ACESULFAME POTASSIUM', score: 'LINKED TO TUMORS' },
+    { name: 'SUCRALOSE', score: 'CAN CAUSE ORGAN SWELLING' },
+    { name: 'AGAVE NECTAR', score: 'CAUSES INSULIN RESISTANCE, LIVER DISEASE' },
+    { name: 'BLEACHED STARCH', score: 'LINKED TO ASTHMA, SKIN IRRITATIONS' },
+    { name: 'TERT BUTYLHYDROQUINONE', score: 'CAN CAUSE STOMACH TUMORS' },
   ];
 
-  // Artificial Food Colorings to Avoid
   const artificialFoodColorings = [
-    'RED #40',
-    'BLUE #1',
-    'BLUE #2',
-    'CITRUS RED #1',
-    'CITRUS RED #2',
-    'GREEN #3',
-    'YELLOW #5',
-    'YELLOW #6',
-    'RED #2',
-    'RED #3',
-    'CARAMEL COLORING',
-    'BROWN HT',
-    'ORANGE B',
-    'BIXIN',
-    'NORBIXIN',
-    'ANNATTO',
+    { name: 'RED #40', score: 'KNOWN CARCINOGEN, HYPERACTIVITY IN CHILDREN' },
+    { name: 'BLUE #1', score: 'CHROMOSOMAL DAMAGE, POTENTIAL CANCER' },
+    { name: 'BLUE #2', score: 'CAN CAUSE BRAIN TUMORS' },
+    { name: 'CITRUS RED #1', score: 'CHROMOSOMAL DAMAGE, POTENTIAL CANCER' },
+    { name: 'CITRUS RED #2', score: 'CAN CAUSE CANCER IF CONSUMED WITH PEEL' },
+    { name: 'GREEN #3', score: 'MAY CAUSE BLADDER TUMORS' },
+    { name: 'YELLOW #5', score: 'MAY CAUSE KIDNEY TUMORS' },
+    { name: 'YELLOW #6', score: 'LINKED TO KIDNEY TUMORS' },
+    { name: 'RED #2', score: 'MAY CAUSE ASTHMA AND CANCER' },
+    { name: 'RED #3', score: 'LINKED TO NERVE DAMAGE, THYROID CANCER' },
+    { name: 'CARAMEL COLORING', score: 'MAY CAUSE CANCER IN MICE' },
+    { name: 'BROWN HT', score: 'CAN CAUSE HYPERACTIVITY, ASTHMA, CANCER' },
+    { name: 'ORANGE B', score: 'BAD FOR LIVER AND BILE DUCT IN HIGH DOSES' },
+    { name: 'BIXIN', score: 'CAN CAUSE HYPERACTIVITY, ASTHMA' },
+    { name: 'NORBIXIN', score: 'CAN CAUSE HYPERACTIVITY, ASTHMA' },
+    { name: 'ANNATTO', score: 'CAN CAUSE HYPERACTIVITY, ASTHMA' },
   ];
 
-  const foundFoodAdditives = foodAdditives.filter((additive) => text.includes(additive));
-  const foundArtificialSweeteners = artificialSweeteners.filter((sweetener) =>
-    text.includes(sweetener)
-  );
-  const foundArtificialFoodColorings = artificialFoodColorings.filter((coloring) =>
-    text.includes(coloring)
-  );
+  const foundFoodAdditives = foodAdditives.filter((additive) => {
+    if (text.includes(additive.name)) {
+      return additive;
+    }
+  });
+
+  const foundArtificialSweeteners = artificialSweeteners.filter((sweetener) => {
+    if (text.includes(sweetener.name)) {
+      return sweetener;
+    }
+  });
+
+  const foundArtificialFoodColorings = artificialFoodColorings.filter((coloring) => {
+    if (text.includes(coloring.name)) {
+      return coloring;
+    }
+  });
 
   const features = {
     foundFoodAdditives,
