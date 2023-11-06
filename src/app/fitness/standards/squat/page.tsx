de1a15f8dcf4702aa7bc565b699d987c0405754e
Ahljenn <aamallari@ucdavis.edu>
2023-11-05 16:33:20 -0800

    fixed build error

--- src/app/fitness/standards/squat/page.tsx
+++ src/app/fitness/standards/squat/page.tsx
@@ -37,36 +37,36 @@ p-12`}
           <p className="mt-5">
             The squat standards below are calculated based on bodyweight and experience level. The
             multipliers used for each category indicate how much of your body weight is considered a
-            good standard for that level. For example, if you're a Beginner, lifting 75% of your
-            body weight in a squat is considered a good starting point. As you progress to higher
-            levels, the multiplier increases, reflecting the higher standards for strength at those
-            levels.
+            good standard for that level. For example, if you&apos;re a Beginner, lifting 75% of
+            your body weight in a squat is considered a good starting point. As you progress to
+            higher levels, the multiplier increases, reflecting the higher standards for strength at
+            those levels.
           </p>
           <ul className="text-white mt-5">
             <li className="mb-2">
-              <strong className="font-bold text-metaPrimary">Beginner:</strong> This level
-              represents a solid starting point for strength training. A multiplier of{' '}
-              <strong className="text-metaPrimary">0.75</strong> means lifting 75% of your body
+              <strong className="font-bold text-metaAccent">Beginner:</strong> This level represents
+              a solid starting point for strength training. A multiplier of{' '}
+              <strong className="text-metaAccent">0.75</strong> means lifting 75% of your body
               weight is considered a good standard for beginners.
             </li>
             <li className="mb-2">
-              <strong className="font-bold text-metaPrimary">Novice:</strong> As you progress, a
-              multiplier of <strong className="text-metaPrimary">1.25</strong> indicates that
-              lifting 125% of your body weight is a commendable goal for novice lifters.
+              <strong className="font-bold text-metaAccent">Novice:</strong> As you progress, a
+              multiplier of <strong className="text-metaAccent">1.25</strong> indicates that lifting
+              125% of your body weight is a commendable goal for novice lifters.
             </li>
             <li className="mb-2">
-              <strong className="font-bold text-metaPrimary">Intermediate:</strong> At this stage, a
-              multiplier of <strong className="text-metaPrimary">1.50</strong> signifies that
-              lifting 150% of your body weight is a noteworthy achievement.
+              <strong className="font-bold text-metaAccent">Intermediate:</strong> At this stage, a
+              multiplier of <strong className="text-metaAccent">1.50</strong> signifies that lifting
+              150% of your body weight is a noteworthy achievement.
             </li>
             <li className="mb-2">
-              <strong className="font-bold text-metaPrimary">Advanced:</strong> Achieving a
-              multiplier of <strong className="text-metaPrimary">2.25</strong> means you've reached
-              an advanced level of strength, lifting 225% of your body weight.
+              <strong className="font-bold text-metaAccent">Advanced:</strong> Achieving a
+              multiplier of <strong className="text-metaAccent">2.25</strong> means you&apos;ve
+              reached an advanced level of strength, lifting 225% of your body weight.
             </li>
             <li>
-              <strong className="font-bold text-metaPrimary">Elite:</strong> The highest level, with
-              a multiplier of <strong className="text-metaPrimary">2.75</strong>, signifies an elite
+              <strong className="font-bold text-metaAccent">Elite:</strong> The highest level, with
+              a multiplier of <strong className="text-metaAccent">2.75</strong>, signifies an elite
               level of strength, lifting an impressive 275% of your body weight.
             </li>
           </ul>
