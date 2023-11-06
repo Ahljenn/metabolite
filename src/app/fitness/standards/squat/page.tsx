3f18a403a22ef32ba3b26cfc9ca8548baedbdf17
Ahljenn <aamallari@ucdavis.edu>
2023-11-06 09:26:32 -0800

    added bench standards

--- src/app/fitness/standards/squat/page.tsx
+++ src/app/fitness/standards/squat/page.tsx
@@ -1,5 +1,4 @@
 import StandardsTable from './../StandardsTable';
-import UnderConstruction from '@/app/components/ui/UnderConstruction';
 import { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Squat Standards',
@@ -13,11 +12,12 @@ const squatMultiplierData: Record<string, number> = {
   Elite: 2.75,
 };
 
+const strengthStandards: number[] = [
+  100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280,
+  290, 300, 310, 320, 330, 340, 350,
+];
+
 const Squat = () => {
-  const strengthStandards: number[] = [
-    100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280,
-    290, 300, 310, 320, 330, 340, 350,
-  ];
   return (
     <section
       className={`flex flex-col items-center  justify-between 
