777a2ab05f689a4dcbb937e847f30f7b139a55c2
Ahljenn <aamallari@ucdavis.edu>
2023-11-08 14:54:19 -0800

    added icons and changed color

--- src/app/fitness/standards/StrengthCards.tsx
+++ src/app/fitness/standards/StrengthCards.tsx
@@ -10,7 +10,7 @@ const StrengthCards = () => {
             className="mt-5 flex flex-col items-center rounded-lg md:flex-row md:max-w-4xl border border-neutral-800 bg-neutral-900 hover:border-metaSecondary transition-all mb-20 md:mb-10"
           >
             <Image
-              className="object-cover w-full rounded-t-lg h-96 md:h-52 md:w-52 md:rounded-none md:rounded-l-lg"
+              className="bg-gray-800 object-cover w-full rounded-t-lg h-96 md:h-52 md:w-52 md:rounded-none md:rounded-l-lg"
               src={card.img}
               alt="calculate"
               width={500}
@@ -39,19 +39,19 @@ const strengthCardData: {
     header: 'Squat Strength Level Standards',
     desc: 'Discover strength level standards for the squat exercise. Compare your performance to others at your body weight.',
     route: '/fitness/standards/squat',
-    img: '/metabolite-m.png',
+    img: '/bbsquat.png',
   },
   {
     header: 'Bench Press Strength Level Standards',
     desc: 'Explore strength level standards for the bench press. Evaluate your performance relative to lifters of similar body weight.',
     route: '/fitness/standards/bench',
-    img: '/metabolite-m.png',
+    img: '/bench.png',
   },
   {
     header: 'Deadlift Strength Level Standards',
     desc: 'Uncover strength level standards for the deadlift. See where your performance ranks compared to others in your weight class.',
     route: '/fitness/standards/deadlift',
-    img: '/metabolite-m.png',
+    img: '/deadlift.png',
   },
 ];
 
