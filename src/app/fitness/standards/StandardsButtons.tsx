f2fabf359dc4c11babd3718d59ec40fca59cedc3
Ahljenn <aamallari@ucdavis.edu>
2023-11-09 09:59:46 -0800

    disable fitness nav on root layout

--- src/app/fitness/standards/StandardsButtons.tsx
+++ src/app/fitness/standards/StandardsButtons.tsx
@@ -1,36 +1,40 @@
 'use client';
+import { usePathname } from 'next/navigation';
 const StandardsButtons = () => {
-  return (
-    <div className="flex flex-col items-center sm:flex-row gap-10 sm:gap-5 justify-center w-full mt-10">
-      <div>
-        <a
-          rel="noopener noreferrer"
-          href="/fitness/standards/squat"
-          className="mt-5 transition-all border rounded-lg py-2 px-4 whitespace-nowrap border-gray-600 bg-neutral-900 hover:border-metaAccent/50"
-        >
-          Squat Standards
-        </a>
+  const currentRoute = usePathname();
+
+  if (currentRoute !== '/fitness/standards')
+    return (
+      <div className="flex flex-col items-center sm:flex-row gap-10 sm:gap-5 justify-center w-full mt-10">
+        <div>
+          <a
+            rel="noopener noreferrer"
+            href="/fitness/standards/squat"
+            className="mt-5 transition-all border rounded-lg py-2 px-4 whitespace-nowrap border-gray-600 bg-neutral-900 hover:border-metaAccent/50"
+          >
+            Squat Standards
+          </a>
+        </div>
+        <div>
+          <a
+            rel="noopener noreferrer"
+            href="/fitness/standards/bench"
+            className="mt-5 transition-all border rounded-lg py-2 px-4 whitespace-nowrap border-gray-600 bg-neutral-900 hover:border-metaAccent/50"
+          >
+            Bench Standards
+          </a>
+        </div>
+        <div>
+          <a
+            rel="noopener noreferrer"
+            href="/fitness/standards/deadlift"
+            className="mt-5 transition-all border rounded-lg py-2 px-4 whitespace-nowrap border-gray-600 bg-neutral-900 hover:border-metaAccent/50"
+          >
+            Deadlift Standards
+          </a>
+        </div>
       </div>
-      <div>
-        <a
-          rel="noopener noreferrer"
-          href="/fitness/standards/bench"
-          className="mt-5 transition-all border rounded-lg py-2 px-4 whitespace-nowrap border-gray-600 bg-neutral-900 hover:border-metaAccent/50"
-        >
-          Bench Standards
-        </a>
-      </div>
-      <div>
-        <a
-          rel="noopener noreferrer"
-          href="/fitness/standards/deadlift"
-          className="mt-5 transition-all border rounded-lg py-2 px-4 whitespace-nowrap border-gray-600 bg-neutral-900 hover:border-metaAccent/50"
-        >
-          Deadlift Standards
-        </a>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default StandardsButtons;
